ca30054649ab8f96a665dbf3b1202d2a888dcad6
yj.gaia <gaiaprotocolnft@gmail.com>
2025-07-18 15:27:29 +0900

    Refactor App component to integrate authentication with RainbowKit and create authentication adapter

--- app/App.tsx
+++ app/App.tsx
@@ -1,43 +1,19 @@
-import { useState } from "react";
-import { Button } from "./components/ui/button";
+import { RainbowKitAuthenticationProvider, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import authenticationAdapter from "./lib/auth";
+import LoginPage from "./login";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("unknown");
+  const AUTHENTICATION_STATUS = 'loading';
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button
-          onClick={() => setCount((count) => count + 1)}
-          aria-label="increment"
-        >
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="card">
-        <Button
-          onClick={() => {
-            fetch("/api/")
-              .then((res) => res.json() as Promise<{ name: string }>)
-              .then((data) => setName(data.name));
-          }}
-          aria-label="get name"
-        >
-          Name from API is: {name}
-        </Button>
-        <p>
-          Edit <code>api/index.ts</code> to change the name
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RainbowKitAuthenticationProvider
+      adapter={authenticationAdapter}
+      status={AUTHENTICATION_STATUS}
+    >
+      <RainbowKitProvider>
+        <LoginPage />
+      </RainbowKitProvider>
+    </RainbowKitAuthenticationProvider>
   );
 }
 
--- app/main.tsx
+++ app/main.tsx
@@ -1,12 +1,12 @@
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createConfig, http, WagmiProvider } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
+import App from './app.tsx'
 import './index.css'
-import '@rainbow-me/rainbowkit/styles.css';
-import LoginPage from './login.tsx'
 
 const queryClient = new QueryClient();
 
@@ -29,9 +29,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <WagmiProvider config={config}>
-        <RainbowKitProvider>
-          <LoginPage />
-        </RainbowKitProvider>
+        <App />
       </WagmiProvider>
     </QueryClientProvider>
   </StrictMode>,
