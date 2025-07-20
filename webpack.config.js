b556a9be213fc0960f2e090ab2245cb1e73833b8
yj.gaia <gaiaprotocolnft@gmail.com>
2025-07-20 19:40:32 +0900

    fix(build): set NODE_ENV in build script and update GAIA_API_URI based on environment

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,11 @@ module.exports = {
     new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
     new MiniCssExtractPlugin({ filename: 'styles.css' }),
     new webpack.DefinePlugin({
-      GAIA_API_URI: JSON.stringify(process.env.GAIA_API_URI)
+      GAIA_API_URI: JSON.stringify(
+        process.env.NODE_ENV === 'production'
+          ? 'https://api.gaia.cc'
+          : 'http://localhost:8080'
+      )
     })
   ],
   mode: 'development'
